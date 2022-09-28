c87381124facaee3afa7a2d5881835595e4448a7
Botlhale Setou <botlhale.setou@gmail.com>
2022-09-28 23:27:38 +0200

    Add/Install linters

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-	devServer: {
+  devServer: {
     static: './dist',
   },
-	plugins: [
+  plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
@@ -14,9 +14,9 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-		clean: true,
+    clean: true,
   },
-	module: {
+  module: {
     rules: [
       {
         test: /\.css$/i,
