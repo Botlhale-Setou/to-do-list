41fff5c935fd30a832b8184da6da69dd6fafb4bd
Botlhale Setou <botlhale.setou@gmail.com>
2022-10-06 22:29:23 +0200

    Complete Add/Remove/Rename function tests

--- src/ToDoLibrary.test.js
+++ src/ToDoLibrary.test.js
@@ -1,7 +1,8 @@
 const { ToDoLibrary } = require('./ToDoLibrary.js');
 
 const x = new ToDoLibrary();
-describe('Add/Remove functions testing', () => {
+
+describe('Add/Remove/Rename functions testing', () => {
   test('if add() adds an item', () => {
     const currNo = x.arrToDos.length;
     x.add('Example task description');
@@ -12,6 +13,13 @@ describe('Add/Remove functions testing', () => {
     x.arrToDos = ['1', '2', '3'];
     const desired = ['1', '3'];
     x.remove(1);
+    expect(x.arrToDos).toEqual(desired);
+  });
+
+  test('if rename() renames the correct item', () => {
+    x.arrToDos = [{ desc: '1' }, { desc: '2' }, { desc: '3' }];
+    const desired = [{ desc: '1' }, { desc: 'two' }, { desc: '3' }];
+    x.rename(1, 'two');
     expect(x.arrToDos).toEqual(desired);
   });
 });
