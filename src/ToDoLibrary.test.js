303c0ff9e02937334ca2248fb5a2814f9dd2ed3b
Botlhale Setou <botlhale.setou@gmail.com>
2022-10-06 21:44:13 +0200

    Test both add and remove funcs

--- src/ToDoLibrary.test.js
+++ src/ToDoLibrary.test.js
@@ -1,9 +1,17 @@
 const { ToDoLibrary } = require('./ToDoLibrary.js');
 
 const x = new ToDoLibrary();
+describe('Add/Remove functions testing', () => {
+  test('if add() adds an item', () => {
+    const currNo = x.arrToDos.length;
+    x.add('Example task description');
+    expect(x.arrToDos.length).toBe(currNo + 1);
+  });
 
-test('if add() adds an item', () => {
-  const currNo = x.arrToDos.length;
-  x.add('Example task description');
-  expect(x.arrToDos.length).toBe(currNo + 1);
+  test('if remove() removes the correct item', () => {
+    x.arrToDos = ['1', '2', '3'];
+    const desired = ['1', '3'];
+    x.remove(1);
+    expect(x.arrToDos).toEqual(desired);
+  });
 });
